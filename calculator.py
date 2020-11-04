9b66d6e851921b514339dd754c4443c40c9b17ef
Antonio P Lacerda <antonioplacerda@gmail.com>
2020-11-04 01:14:26 +0000

    create test for empty string

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+def add():
     pass
+
+class TestStringMethods(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(add(), 0)
 
 if __name__ == '__main__':
     unittest.main()
